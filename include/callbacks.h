0ad0c9c67709c443e4592b842f7af30ee17af899
tagener-noisu <op4.renegat@gmail.com>
2017-06-11 13:38:10 +0600

    Remove unused callback declaration

--- include/callbacks.h
+++ include/callbacks.h
@@ -3,8 +3,6 @@
 #include <ev.h>
 //-------------------------------------------------------------------
 
-void send_cb (struct ev_loop *loop, ev_io *w, int revents);
-
 void accept_cb (struct ev_loop *loop, ev_io *w, int revents);
 
 void sigint_cb(struct ev_loop *loop, ev_signal *w, int revents);
